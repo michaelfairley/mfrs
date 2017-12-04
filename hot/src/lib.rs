0f99d37ca4a49d0ad0de0ab512c13d9c2ba976c8
Michael Fairley <michaelfairley@gmail.com>
2017-12-03 20:48:23 -0400

    Copy dylib before opening it since it's gotten harder to get dlclose to let go of my dylibs

--- hot/src/lib.rs
+++ hot/src/lib.rs
@@ -1,5 +1,6 @@
 extern crate notify;
 extern crate libloading;
+extern crate tempdir;
 
 use std::sync::mpsc;
 use std::ffi::OsString;
@@ -7,6 +8,7 @@ use std::ffi::OsString;
 pub struct State();
 
 pub struct Library {
+  tempdir: tempdir::TempDir,
   dylib_path: OsString,
   _watcher: notify::RecommendedWatcher,
   rx: mpsc::Receiver<notify::RawEvent>,
@@ -14,6 +16,7 @@ pub struct Library {
 }
 
 pub struct Instance {
+  name: OsString,
   _raw: libloading::Library,
   pub init_fn: libloading::os::unix::Symbol<extern fn() -> Box<State>>,
   pub tick_fn: libloading::os::unix::Symbol<extern fn(&mut State) -> bool>,
@@ -21,14 +24,33 @@ pub struct Instance {
 }
 
 impl Instance {
-  fn load(dylib_path: &OsString) -> Self {
-    let raw = libloading::Library::new(dylib_path).expect("Couldn't find dylib");
+  fn load(
+    dylib_path: &OsString,
+    tempdir: &tempdir::TempDir,
+  ) -> Self {
+    let now = std::time::SystemTime::now().duration_since(std::time::UNIX_EPOCH).unwrap();
+    let now_in_ms = now.as_secs() * 1000 + now.subsec_nanos() as u64 / 1_000_000;
+    let name = format!("{}.dylib", now_in_ms).into();
+
+    let new_path = tempdir.path().join(&name);
+
+    std::fs::copy(dylib_path, &new_path).unwrap();
+
+    let output = std::process::Command::new("install_name_tool")
+      .arg("-id").arg(&new_path).arg(&new_path)
+      .output()
+      .unwrap();
+
+    if !output.status.success() { panic!(output); }
+
+    let raw = libloading::Library::new(new_path).expect("Couldn't find dylib");
 
     let init_fn = init_fn(&raw);
     let tick_fn = tick_fn(&raw);
     let cleanup_fn = cleanup_fn(&raw);
 
     Self{
+      name,
       _raw: raw,
       init_fn,
       tick_fn,
@@ -46,13 +68,19 @@ impl Library {
 
     watcher.watch(&dylib_path, notify::RecursiveMode::NonRecursive).unwrap();
 
-    let instance = Instance::load(&dylib_path);
+    let tempdir = tempdir::TempDir::new_in(
+      AsRef::<std::path::Path>::as_ref(&dylib_path).parent().unwrap(),
+      AsRef::<std::path::Path>::as_ref(&dylib_path).file_name().unwrap().to_str().unwrap(),
+    ).unwrap();
+
+    let instance = Instance::load(&dylib_path, &tempdir);
 
     Library {
       dylib_path: dylib_path,
       instance: instance,
       rx: rx,
       _watcher: watcher,
+      tempdir: tempdir,
     }
   }
 
@@ -63,7 +91,11 @@ impl Library {
   fn do_reload(&mut self) {
     println!("Reloading!");
 
-    self.instance = Instance::load(&self.dylib_path);
+    let prev_instance = std::mem::replace(&mut self.instance, Instance::load(&self.dylib_path, &self.tempdir));
+
+    let prev_path = self.tempdir.path().join(&prev_instance.name);
+
+    std::fs::remove_file(prev_path).unwrap();
   }
 
   pub fn reload_block(&mut self) {
